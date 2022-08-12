8c8830fce8492aecc6c09725be55aaf43fd43a84
inevitable360 <pvidalx@gmail.com>
2022-08-13 00:19:49 +0100

    Libdogecoin for PHP v0.69
    Libdogecoin for PHP v0.69
    Firstr elease :P

---
+++ example.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*   File: Functions used on the Libdogecoin
+*   Description: Bind Libdogecoin to PHP using c compiled that prints in JSON format
+*
+*   Available Commands:
+*    generatePrivPubKeypair
+*    generateHDMasterPubKeypair
+*    generateDerivedHDPubkey
+*    verifyPrivPubKeypair
+*    verifyHDMasterPubKeypair
+*    verifyP2pkhAddress
+*    start_transaction
+*/
+
+    function libdogecoin ($commands){
+      exec('./php-wrappers/json-php/libdogecoin-json-php '.$commands[0].' 2>&1', $output, $retval);
+      return json_decode($output[0]);
+    }
+
+    $commands[] = "generatePrivPubKeypair"; //command
+    //$commands[1] = ""; // hash_doge (tx hash with doge)
+    //$commands[2] = ""; // external_p2pkh_addr
+    //$commands[3] = ""; // numeric doge to send ex: 5.0
+    //$commands[4] = ""; // numeric fees ex: 0.00226
+    //$commands[5] = ""; // numeric tx index
+    //$commands[6] = ""; // mypubkey
+    //$commands[7] = ""; // myscriptpubkey
+    //$commands[8] = ""; // myprivkey
+
+    // Simple exemple
+    echo "This is my Private Key: ";
+    echo libdogecoin($commands)->private;
+
+    echo "<br>";
+
+    echo "This is my Public Key";
+    echo libdogecoin($commands)->public;
+?>
